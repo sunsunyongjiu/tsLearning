add962dc6e518baff5f15bf9c47663874a1d6844
sunshiyi <shiyi.sun@htwins.com.cn>
2018-03-16 14:25:07 +0800

    feat: 初始化

---
+++ src/index.js
@@ -0,0 +1,4 @@
+var myName = "\u6D4B\u8BD5";
+var age = 26;
+var me = myName + "\u4ECA\u5E74" + age + "\u4E86";
+console.log(me);
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let myName:string=`测试`;
+let age:number=26;
+let me:string=`${myName}今年${age}了`
+console.log(me)
